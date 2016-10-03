ec4375cf1e140515b39acc31d49be1b256895695
dagmawi <doitdagi@gmail.com>
2016-10-03 16:19:54 +0200

    add initial implemenation of a link, to open node-red dashboard node graph inside osjs

---
+++ main.js
@@ -0,0 +1,36 @@
+
+(function(Application, GUI, Dialogs, Utils, API, VFS) {
+  'use strict';
+
+  /////////////////////////////////////////////////////////////////////////////
+  // APPLICATION
+  /////////////////////////////////////////////////////////////////////////////
+
+  function AppAgileNodeRedDashBoard(args, metadata) {
+    Application.apply(this, ['AppAgileNodeRedDashBoard', args, metadata, {
+      src: '',
+      title: metadata.name,
+      icon: metadata.icon,
+      width: 960,
+      height: 640,
+      allow_resize: true,
+      allow_restore: true,
+      allow_maximize: true
+    }]);
+
+     var src = document.location.protocol + "//" + document.location.hostname + ":2000";
+
+    this.options.src = src;
+  }
+
+  AppAgileNodeRedDashBoard.prototype = Object.create(Application.prototype);
+
+  /////////////////////////////////////////////////////////////////////////////
+  // EXPORTS
+  /////////////////////////////////////////////////////////////////////////////
+
+  OSjs.Applications = OSjs.Applications || {};
+  OSjs.Applications.AppAgileNodeRedDashBoard = OSjs.Applications.AppAgileNodeRedDashBoard || {};
+  OSjs.Applications.AppAgileNodeRedDashBoard.Class = AppAgileNodeRedDashBoard;
+
+})(OSjs.Helpers.IFrameApplication, OSjs.GUI, OSjs.Dialogs, OSjs.Utils, OSjs.API, OSjs.VFS);
